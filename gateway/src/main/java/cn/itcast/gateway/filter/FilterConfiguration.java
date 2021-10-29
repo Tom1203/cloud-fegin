1c224050aef82611180b25e2921de096e53e6f15
tho <sdsa.com>
2021-10-29 20:26:48 +0800

    SpringCloud-day2

---
+++ gateway/src/main/java/cn/itcast/gateway/filter/FilterConfiguration.java
@@ -0,0 +1,49 @@
+package cn.itcast.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+public class FilterConfiguration {
+
+    @Bean
+    @Order(-2)
+    public GlobalFilter globalFilter1(){
+        return (((exchange, chain) -> {
+            log.info("过滤器1的pre 阶段");
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                log.info("过滤器1的post 阶段");
+            }));
+        }));
+    }
+
+
+    @Bean
+    @Order(-1)
+    public GlobalFilter globalFilter2(){
+        return (exchange, chain) -> {
+            log.info("过滤器2的pre 阶段");
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                log.info("过滤器2的post 阶段");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(0)
+    public GlobalFilter globalFilter3(){
+        return ((exchange, chain) -> {
+            log.info("过滤器3的pre阶段！");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("过滤器3的post阶段！");
+            }));
+        });
+    }
+
+}
